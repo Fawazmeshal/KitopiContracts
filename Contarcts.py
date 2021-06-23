356f5728b87037b6bad71b279cfd812f653cf1a3
Fawazmeshal <86286159+Fawazmeshal@users.noreply.github.com>
2021-06-23 10:08:57 +0300

    Update Contarcts.py

--- Contarcts.py
+++ Contarcts.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 
 st.title('Hello, my name is fawaz and i creat a dashboard')
+
+
+excel= 'C:/Users/Fawaz Almutairi/Desktop/Contractss.xlsx'
+df=pd.read_excel(excel, usecols='A:D', sheet_name=0, index_col=0)
+st.table(df)
