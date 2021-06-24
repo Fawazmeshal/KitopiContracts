e6b2f1d1f4c3b229cc913101ba874a5d76c1a820
Fawazmeshal <86286159+Fawazmeshal@users.noreply.github.com>
2021-06-24 11:58:10 +0300

    Update Contarcts.py

--- Contarcts.py
+++ Contarcts.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 st.title('Hi Kitopi')
 
-excel='C:/Users/Fawaz Almutairi/Desktop/Contractss.xlsx'
+excel='C:\Users\Fawaz Almutairi\Desktop\Contractss.xlsx'
 df= pd.read_excel(excel)
 
 st.table(df)
